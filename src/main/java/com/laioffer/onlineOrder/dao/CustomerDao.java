82e97e39717043f0174726b525a1df7231bde849
Jiayu0330 <jiayu.jiang@centre.edu>
2021-09-13 20:32:58 -0700

    first commit

---
+++ src/main/java/com/laioffer/onlineOrder/dao/CustomerDao.java
@@ -0,0 +1,53 @@
+package com.laioffer.onlineOrder.dao;
+
+import com.laioffer.onlineOrder.entity.Authorities;
+import com.laioffer.onlineOrder.entity.Customer;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void signUp(Customer customer) {
+        Authorities authorities = new Authorities();
+        authorities.setAuthorities("ROLE_USER");
+        authorities.setEmail(customer.getEmail());
+
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction(); // begin a transaction
+            session.save(authorities);
+            session.save(customer);
+            session.getTransaction().commit(); // end the transaction
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public Customer getCustomer(String email) {
+        Customer customer = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            customer = session.get(Customer.class, email); // similar to "select from Customer where email = email"
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return customer;
+    }
+}
+
